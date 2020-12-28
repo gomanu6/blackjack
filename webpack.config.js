752bef41001d615f62f8877f8939596e538dd472
Manu <gomanu6@gmail.com>
2020-12-28 21:45:04 +0530

    Configuring the Webpack config file for starting the project

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,82 @@
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const path = require("path");
 
 module.exports = {
 	mode: "development",
 	entry: {
-		main: {},
-		app: {},
+		main: {
+			import: "./src/index.js",
+			dependOn: "shared",
+		},
+		app: {
+			import: "./src/app.js",
+			dependOn: "shared",
+		},
 		shared: ["react", "react-dom"],
 	},
 	output: {
 		path: path.resolve(__dirname, "dist"),
 		filename: "[name].[bundle].js",
+		assetModuleFilename: "images/[hash][ext][query]",
 	},
 	devServer: {
 		open: true,
 		contentBase: "./dist",
+		hot: true,
 	},
+	optimization: {
+		runtimeChunk: "single",
+		moduleIds: "deterministic",
+		splitChunks: {
+			chunks: "all",
+		},
+		cacheGroups: {
+			vendor: {
+				test: /[\\/]node_modules[\\/]/,
+				name: "vendors",
+				chunks: "all",
+			},
+		},
+	},
+	module: {
+		rules: [
+			{
+				test: /\.css$/i,
+				use: [MiniCssExtractPlugin.loader, "css-loader"],
+				include: path.resolve(__dirname, "src"),
+			},
+			{
+				test: /\.(htm|html)$/i,
+				type: "asset/resource",
+				include: path.resolve(__dirname, "src"),
+			},
+			{
+				test: /\.js$/,
+				include: path.resolve(__dirname, "src"),
+				loader: "babel-loader",
+			},
+			{
+				test: /\.(png|svg|jpg|jpeg|gif)$/i,
+				include: path.resolve(__dirname, "src"),
+				type: "asset/resource",
+			},
+			{
+				test: /\.(woff|woff2|eot|ttf|otf)$/i,
+				include: path.resolve(__dirname, "src"),
+				type: "asset/resource",
+			},
+		],
+	},
+	plugins: [
+		new MiniCssExtractPlugin(),
+		new HtmlWebpackPlugin({
+			template: "./src/template.html",
+			filename: "index.html",
+		}),
+		new CleanWebpackPlugin({
+			cleanStaleWebpackAssets: false,
+		}),
+	],
 };
