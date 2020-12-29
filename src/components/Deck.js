bb73e612a97bab910a967f1b4158d6058a0e7000
Manu <gomanu6@gmail.com>
2020-12-29 23:35:14 +0530

    testing the deck generation and shuffle

---
+++ src/components/Deck.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Deck = () => {
+	let deck = [];
+
+	let suites = ["Spade", "Heart", "Clubs", "Diamond"];
+
+	let currentCard = {};
+
+	for (let i = 0; (i = suites.length); i++) {
+		console.log(i);
+	}
+
+	return (
+		<div>
+			<div></div>
+		</div>
+	);
+};
+export default Deck;
